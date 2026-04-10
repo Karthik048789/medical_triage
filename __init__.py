5da1334dcdd745d0053e7c5cab1b3bb214fa4a18
Karthik S <149348762+Karthik048789@users.noreply.github.com>
2026-04-10 21:36:09 +0530

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-"""Medical Triage OpenEnv Environment."""
-
-from .models import TriageAction, TriageObservation, TriageState
-from .client import MedicalTriageEnv
-
-__all__ = ["TriageAction", "TriageObservation", "TriageState", "MedicalTriageEnv"]
